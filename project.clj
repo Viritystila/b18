6512d80f162a34cea894802cd9d43f2678bfcd7c
Mikael Reponen <mikael.reponen@gmail.com>
2020-01-13 00:31:57 +0200

    video testing with music

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
                     [trigger "0.1.0-SNAPSHOT"]
                     [org.viritystila/cutter "0.0.1-SNAPSHOT"]
                     [rm-hull/markov-chains "0.1.1"]]
-                    
+     :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
      :jvm-opts ^:replace [])
